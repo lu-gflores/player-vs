599830fb099a1424fb3ebff85e5cf2aef333f1e1
George Flores <georgeflores886@gmail.com>
2021-04-28 10:28:21 -0400

    created sub folder for tourneys pages

---
+++ pages/tournaments/[id].js
@@ -0,0 +1,8 @@
+import Layout from '@/components/Layout'
+export default function TournamentPage() {
+    return (
+        <Layout>
+            <h1>My Tournament</h1>
+        </Layout>
+    )
+}
---
+++ pages/tournaments/index.js
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout'
+
+export default function Tournaments() {
+    return (
+        <Layout>
+            <h1>My Tournaments</h1>
+        </Layout>
+    )
+}
