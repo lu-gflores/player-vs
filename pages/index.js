3a644e37a62f91e15f00e4bdff4eada6c349a3ca
George Flores <georgeflores886@gmail.com>
2021-06-10 14:27:55 -0400

    rendered tournmanets in home index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import TournamentItem from '@/components/TournamentItem'
+import { API_URL } from '@/config/index'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+  console.log(events)
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Tournaments</h1>
+      {events.length === 0 && <h2>No Tournaments available right now</h2>}
+      {events.map(tr => (
+        <TournamentItem key={tr.id} tour={tr} />
+      ))}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {
+      events
+    },
+    revalidate: 1
+  }
 }
